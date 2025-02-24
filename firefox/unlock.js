5f4b0131911fa3ee641dff3faca0c0b0e7f6a57a
Alex Turner <aturner.xyz@gmail.com>
2025-02-24 17:11:29 -0600

    Firefox

---
+++ firefox/unlock.js
@@ -0,0 +1,30 @@
+const OVERLAY_IDS = ['HardsellOverlay', 'ContentWallHardsell'];
+
+const unlockPage = () => {
+	document.body.style.overflow = '';
+	document.body.style.position = '';
+	window.onscroll = () => {};
+}
+
+for (id of OVERLAY_IDS) {
+	const overlay = document.getElementById(id);
+	if (overlay) {
+		overlay.remove();
+		unlockPage();
+	}
+}
+
+new MutationObserver((mutations) => {
+	for (const mutation of mutations) {
+		if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+			const node = mutation.addedNodes.item(0);
+			if (OVERLAY_IDS.includes(node.id)) {
+				node.remove();
+			}
+		}
+		else if (mutation.type === 'attributes' && document.body.style.overflow === 'hidden') {
+			unlockPage();
+			return;
+		}
+	}
+}).observe(document.body, { childList: true, attributes: true, attributeFilter: ['style'] });
